06d83ab7d32f8e93ed9da60b9bf56bdb9f7323cc
Jeet Bubna <bubnajeet@gmail.com>
2026-01-22 17:41:43 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,19 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+// returns the command to be evaluated
+char *read_command(){
+  char buffer[100];
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  fgets(buffer, 100, stdin);
 
-  char text[100];
+  buffer[strcspn(buffer, "\n")] = '\0';
 
-  fgets(text, 100, stdin);
-  text[strlen(text) - 1] = '\0';
+  char *text = malloc(strlen(buffer) + 1 ); // +1 because strlen doenst inclde null terminator '\0'
+  strcpy(text, buffer);
 
+  return text;
+}
+
+// default command for now, which is just command not found
+int default_command(char *text){
   printf("%s: command not found\n", text);
+  return 0;
+}
+
+// parses and executes the command
+int eval(char *text){
+  // parsing
+  char command[] = "default";
+
+  // executing
+  default_command(text);
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  
+  while(1){
+    // Flush after every printf
+    setbuf(stdout, NULL);
+
+    // REPL loop
+    char *text = read_command();
+    eval(text);
+    free(text);
+  }
 
   return 0;
 }
